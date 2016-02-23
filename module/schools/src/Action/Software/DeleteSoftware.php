1475f7ae70d3469d290ec589da493c77ef840097
Georgios Tsakalos <ozmax.planet@gmail.com>
2016-02-23 18:14:54 +0200

    add post and delete actions
    update bootstrap file

---
+++ module/schools/src/Action/Software/DeleteSoftware.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * gredu_labs.
+ *
+ * @link https://github.com/eellak/gredu_labs for the canonical source repository
+ *
+ * @copyright Copyright (c) 2008-2015 Greek Free/Open Source Software Society (https://gfoss.ellak.gr/)
+ * @license GNU GPLv3 http://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
+
+namespace GrEduLabs\Schools\Action\Software;
+
+use Exception;
+use GrEduLabs\Schools\Service\SoftwareServiceInterface;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class DeleteSoftware
+{
+    /**
+     *
+     * @var SoftwareServiceInterface
+     */
+    private $softwareService;
+
+    public function __construct(SoftwareServiceInterface $softwareService)
+    {
+        $this->softwareService = $softwareService;
+    }
+
+    public function __invoke(Request $req, Response $res)
+    {
+        $school = $req->getAttribute('school', false);
+        if (!$school->id) {
+            return $res->withStatus(403, 'No school');
+        }
+        $id = $req->getParam('id', false);
+
+        if (!$id) {
+            $res = $res->withStatus(404);
+
+            return $res;
+        }
+
+        $software = $this->softwareService->getSoftwareById($id);
+        if ($teacher['school_id'] != $school->id) {
+            $res = $res->withStatus(403, 'Schools not match');
+
+            return $res;
+        }
+
+        try {
+            $this->softwareService->removeSoftware($id);
+            $res = $res->withStatus(204);
+        } catch (Exception $ex) {
+            $res = $res->withStatus(500, $ex->getMessage());
+        }
+
+        return $res;
+    }
+}
---
+++ module/schools/src/Action/Software/PersistSoftware.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * gredu_labs.
+ *
+ * @link https://github.com/eellak/gredu_labs for the canonical source repository
+ *
+ * @copyright Copyright (c) 2008-2015 Greek Free/Open Source Software Society (https://gfoss.ellak.gr/)
+ * @license GNU GPLv3 http://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
+
+namespace GrEduLabs\Schools\Action\Software;
+
+use Exception;
+use GrEduLabs\Schools\Service\SoftwareServiceInterface;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class PersistSoftware
+{
+    /**
+     *
+     * @var SoftwareServiceInterface
+     */
+    private $softwareService;
+
+    public function __construct(SoftwareServiceInterface $softwareService)
+    {
+        $this->softwareService = $softwrareService;
+    }
+
+    public function __invoke(Request $req, Response $res, array $args = [])
+    {
+        $school = $req->getAttribute('school', false);
+        if (!$school) {
+            return $res->withStatus(403, 'No school');
+        }
+        $params              = $req->getParams();
+        $params['school_id'] = $school->id;
+        $id                  = $params['id'];
+        unset($params['id']);
+
+        try {
+            if ($id) {
+                $software= $this->softwareService->updateSoftware($params, $id);
+                $res     = $res->withStatus(200);
+            } else {
+                $software= $this->softwareService->createSoftware($params);
+                $res     = $res->withStatus(201);
+            }
+            $res = $res->withJson($software);
+        } catch (Exception $ex) {
+            $res = $res->withStatus(500, $ex->getMessage());
+        }
+
+        return $res;
+    }
+}
