2daa66d03f7f90302bf38abaa831435232631754
Vassilis Kanellopoulos <contact@kanellov.com>
2016-03-23 13:01:09 +0200

    remove delete app form middleware

--- module/debug/bootstrap.php
+++ module/debug/bootstrap.php
@@ -36,16 +36,4 @@ return function (Slim\App $app) {
             return $logger;
         });
     }, -10);
-
-//    $events('on', 'app.bootstrap', function ($app, $container) {
-//        $container->get('router')->getNamedRoute('application_form')->add(function ($req, $res, $next) {
-//            $school_id = $req->getAttribute('school')->id;
-//            $appForm = RedBeanPHP\R::findOne('applicationform', 'school_id = ?', [$school_id]);
-//            if ($appForm) {
-//                RedBeanPHP\R::trash($appForm);
-//            }
-//
-//            return $next($req, $res);
-//        });
-//    }, -10);
 };
