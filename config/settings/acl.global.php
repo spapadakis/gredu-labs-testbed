0c6e1482bb25ccb61b774f2048a39d91d770e123
Stavros Papadakis <spapad@gmail.com>
2016-10-10 10:38:58 +0300

    Work in progress

--- config/settings/acl.global.php
+++ config/settings/acl.global.php
@@ -39,6 +39,7 @@ return [
                 ['/in_numbers', ['guest', 'user'], ['get']],
                 ['/export/csv/edulabs_{type}.csv', ['guest', 'user'], ['get']],
                 ['/open-data', ['guest', 'user'], ['get']],
+                ['/admin', ['admin'], ['get']],
             ],
         ],
     ],
