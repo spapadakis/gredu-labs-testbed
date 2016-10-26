5af3565e888352f4bb87f03c6c85726701b1b40c
Aspasia Katsi <aspasia@aspasia.minedu.gov.gr>
2016-10-26 12:48:02 +0300

    mew migration

--- module/application_form/data/import_approved.sql
+++ module/application_form/data/import_approved.sql
@@ -4,16 +4,31 @@ id INT NOT NULL AUTO_INCREMENT ,
 PRIMARY KEY ( id )
 );
 
+CREATE TABLE approvedapplications(
+id INT NOT NULL AUTO_INCREMENT ,
+submitted int,
+PRIMARY KEY ( id )
+);
+
 LOAD DATA LOCAL INFILE '/tmp/schapproved.csv' 
 INTO TABLE approved_ids 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
 
+INSERT INTO approvedapplications
+SELECT id, max(submitted)
+FROM applicationform
+WHERE school_id IN (
+SELECT id
+FROM approved_ids)
+group by school_id;
 
 UPDATE applicationform SET approved =1,approved_date = curdate() WHERE id IN (
 SELECT id
-FROM approved_ids
+FROM approvedapplications
 );
 
 DROP TABLE approved_ids;
+
+DROP TABLE approvedapplications
